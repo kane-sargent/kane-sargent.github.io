26ed49c84ad903e22d9aef9ce34e8a34947c998e
kane-sargent <kane@hive.run>
2024-10-15 19:00:32 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://kane-sargent.github.io' ,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kane-sargent/kane-sargent.github.io',
+  baseUrl: 'kane-sargent/kane-sargent.github.io',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
